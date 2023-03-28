e4665aa73b633e0f5c6137ae08b838df81e83972
Simeon <simeons2002@gmail.com>
2023-03-28 13:50:37 +0200

    Create files and write basic math functions
    The basic math funciton add, substract, multiple and divide were created

---
+++ script.js
@@ -0,0 +1,16 @@
+// FUNCTIONS SCOPE
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function substract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function division(num1, num2) {
+  return num1 / num2;
+}
