e047eed87a165d4cf8ca73b2d443ee2097d0a2bd
Simeon <simeons2002@gmail.com>
2023-04-02 22:57:21 +0300

    Add divided by zero message and restriction to only one decimal point

--- script.js
+++ script.js
@@ -32,10 +32,20 @@ function showAndStoreNumber(e) {
   const para = document.querySelector('.calc-display p');
   para.textContent = '';
   if (isFirst) {
+    if (hasDecimal && e.target.textContent === '.') {
+      para.textContent = firstNum;
+      return;
+    }
+    if (e.target.textContent === '.') hasDecimal = true;
     firstNum += e.target.textContent;
     para.textContent = firstNum;
   } else {
     firstNum = result;
+    if (hasDecimal && e.target.textContent === '.') {
+      para.textContent = secondNum;
+      return;
+    }
+    if (e.target.textContent === '.') hasDecimal = true;
     secondNum += e.target.textContent;
     para.textContent = secondNum;
   }
@@ -50,10 +60,16 @@ function evaluateResultAtStart(e) {
   }
 }
 
+function displayResult() {
+  const para = document.querySelector('.calc-display p');
+  para.textContent = result === Infinity ? 'NASTY ERROR' : result;
+}
+
 // MAIN SCOPE
 let firstNum = '';
 let secondNum = '';
 let isFirst = true;
+let hasDecimal = false;
 let result;
 let operator;
 
@@ -65,14 +81,15 @@ calcButtons.forEach((button) => {
 const operatorButtons = document.querySelectorAll('.operator');
 operatorButtons.forEach((button) => {
   button.addEventListener('click', (e) => {
-    const para = document.querySelector('.calc-display p');
     if (isFirst) {
       result = evaluateResultAtStart(e);
       isFirst = false;
     } else result = operate(operator, +firstNum, +secondNum);
+    console.log(result, firstNum, secondNum);
     secondNum = ''; //resetting the second number.
+    hasDecimal = false;
     operator = e.target.textContent; //setting operator to new value
-    para.textContent = result; //displaying result
+    displayResult();
   });
 });
 
@@ -80,5 +97,17 @@ const evalButton = document.querySelector('.evaluator');
 evalButton.addEventListener('click', (e) => {
   if (isFirst) return;
   result = operate(operator, +firstNum, +secondNum);
-  document.querySelector('.calc-display p').textContent = result;
+  console.log(result, firstNum, secondNum);
+  displayResult();
+});
+
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', (e) => {
+  //resetting everything (including isFirst again)
+  isFirst = true;
+  firstNum = '';
+  secondNum = '';
+  operator = '';
+  const para = document.querySelector('.calc-display p');
+  para.textContent = '';
 });
